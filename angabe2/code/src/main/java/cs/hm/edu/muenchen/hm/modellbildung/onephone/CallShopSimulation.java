f4504eb8f906b0f5e4de69a67b17470298e04ba2
Arne2 <arne.schoentag@imagefiguren.de>
2017-05-08 18:23:10 +0200

    configuration via -arguments

--- angabe2/code/src/main/java/cs/hm/edu/muenchen/hm/modellbildung/onephone/CallShopSimulation.java
+++ angabe2/code/src/main/java/cs/hm/edu/muenchen/hm/modellbildung/onephone/CallShopSimulation.java
@@ -7,6 +7,8 @@ import cs.hm.edu.muenchen.hm.modellbildung.onephone.events.ArrivalEvent;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
 
 import static cs.hm.edu.muenchen.hm.modellbildung.onephone.config.CallShopConfiguration.*;
 
@@ -53,14 +55,41 @@ public class CallShopSimulation {
         state.events.add(event);
     }
 
-    public static void main(String[] args) {
-        if (args.length == 5) {
-            MEAN_ARRIVAL = Integer.parseInt(args[0]);
-            MEAN_CALL = Integer.parseInt(args[1]);
-            DURATION = Integer.parseInt(args[2]);
-            VIP_PERCENTAGE = Integer.parseInt(args[3]);
-            CONFIGURATION = Integer.parseInt(args[4]);
+    /**
+     * Sets the constants of the Configuration at the begin of the program.
+     * @param args array of strings
+     */
+    private static void loadConfiguration(String[] args){
+        List<String> arguments = Arrays.asList(args);
+        int index = arguments.indexOf("-a");
+        if (index >= 0 && arguments.size() > index){
+            String s = arguments.get(index+1);
+            MEAN_ARRIVAL = Integer.parseInt(s);
         }
+        index = arguments.indexOf("-c");
+        if (index >= 0 && arguments.size() > index){
+            String s = arguments.get(index+1);
+            MEAN_CALL = Integer.parseInt(s);
+        }
+        index = arguments.indexOf("-d");
+        if (index >= 0 && arguments.size() > index){
+            String s = arguments.get(index+1);
+            DURATION = Integer.parseInt(s);
+        }
+        index = arguments.indexOf("-vip");
+        if (index >= 0 && arguments.size() > index){
+            String s = arguments.get(index+1);
+            VIP_PERCENTAGE = Integer.parseInt(s);
+        }
+        index = arguments.indexOf("-conf");
+        if (index >= 0 && arguments.size() > index){
+            String s = arguments.get(index+1);
+            CONFIGURATION = Integer.parseInt(s);
+        }
+    }
+
+    public static void main(String[] args) {
+        loadConfiguration(args);
 
         final Path folder = Paths.get("../data/");
         try (final SimulationState state = new SimulationState(folder)) {

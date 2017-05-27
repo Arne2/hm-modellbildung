1c1e4ab94857bdbb2ef16cef10491928e81da579
peter-mueller <peter7995@gmail.com>
2017-05-27 16:23:01 +0200

    better console view

--- angabe3/code/src/main/java/time/events/PersonMoveEvent.java
+++ angabe3/code/src/main/java/time/events/PersonMoveEvent.java
@@ -1,13 +1,14 @@
 package time.events;
 
+import field.Field;
 import field.Fields;
 import field.location.Location;
+import field.location.Locations;
 import main.State;
 import person.Person;
 
 import java.math.BigDecimal;
-import java.util.Comparator;
-import java.util.Set;
+import java.util.*;
 
 /**
  * @author peter-mueller
@@ -15,23 +16,49 @@ import java.util.Set;
 public class PersonMoveEvent extends BaseEvent {
 
     private final Person person;
+    private final State state;
 
     public PersonMoveEvent(BigDecimal time, State state, Person person) {
-        super(time, state);
+        super(time);
+        this.state = state;
         this.person = person;
     }
 
     @Override
-    public void execute() {
-        final Location center = person.getLocation();
-        final Set<Location> moore = Fields.moore(getState().field, center);
+    public List<Event> execute() {
+        final Location center = state.field.locationOf(person);
+        final Set<Location> moore = Fields.moore(state.field, center);
 
         final Location bestTarget = moore.stream()
-                .filter(getState().field::isFree)
-                .min(Comparator.comparingDouble(getState().use::get))
+                .filter(state.field::isFree)
+                .max(Comparator.comparingDouble(state.use::get))
                 .orElse(null);
 
-        person.moveTo(bestTarget);
 
+        final List<Event> newEvents = new ArrayList<>();
+
+        if (bestTarget == null) {
+            //TODO how long to wait if no move possible?
+            final BigDecimal timeForMove = BigDecimal.valueOf(1 / person.getVelocity());
+            final BigDecimal nextMove = state.clock.systemTime().add(timeForMove);
+            final PersonMoveEvent event = new PersonMoveEvent(nextMove, state, person);
+            newEvents.add(event);
+            return newEvents;
+        }
+
+        if (state.field.isTarget(bestTarget)) {
+            state.field.removePerson(person);
+            return newEvents;
+        }
+
+        state.field.putPerson(person, bestTarget);
+        final double distance = Locations.distance(center, bestTarget);
+        final BigDecimal timeForMove = BigDecimal.valueOf(distance / person.getVelocity());
+        final BigDecimal nextMove = state.clock.systemTime().add(timeForMove);
+        final PersonMoveEvent event = new PersonMoveEvent(nextMove, state, person);
+        newEvents.add(event);
+
+
+        return newEvents;
     }
 }

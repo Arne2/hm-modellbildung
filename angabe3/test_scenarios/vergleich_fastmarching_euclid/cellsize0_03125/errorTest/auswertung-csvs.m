69597693e05371bdc0f4d7bf5d56470ef6f6ce6b
Dima <dimitrie.diez@gmx.de>
2017-06-23 12:13:20 +0200

    3D Abbildung in Doku eingefügt

---
+++ angabe3/test_scenarios/vergleich_fastmarching_euclid/cellsize0_03125/errorTest/auswertung-csvs.m
@@ -0,0 +1,28 @@
+(* ::Package:: *)
+
+SetDirectory[FileNameJoin@{NotebookDirectory[], "/"}]
+outputDir =  FileNameJoin@{NotebookDirectory[], "/"};
+Load[name_]:= Import[FileNameJoin@{NotebookDirectory[], name <> ".csv"},HeaderLines->1];
+
+
+
+euclid = Import[FileNameJoin@{NotebookDirectory[], "euclid" <> ".csv"},HeaderLines->1];
+fastmarching = Import[FileNameJoin@{NotebookDirectory[], "fastmarching" <> ".csv"},HeaderLines->1];
+error = Import[FileNameJoin@{NotebookDirectory[], "error" <> ".csv"},HeaderLines->1];
+
+
+(*ListPlot3D@euclid
+Export[FileNameJoin@{outputDir,"Euklid.pdf"},%];
+ListPlot3D@fastmarching
+Export[FileNameJoin@{outputDir,"FastMarching.pdf"},%];*)
+ListPlot3D[error, AxesLabel->{y-Richtung, x-Richtung, AbsError [m]}]
+Export[FileNameJoin@{outputDir,"Error.pdf"},%];
+
+
+
+
+
+
+
+
+
--- angabe3/test_scenarios/vergleich_fastmarching_euclid/test/auswertung-csvs.m
+++ angabe3/test_scenarios/vergleich_fastmarching_euclid/test/auswertung-csvs.m
@@ -11,22 +11,15 @@ fastmarching = Import[FileNameJoin@{NotebookDirectory[], "fastmarching" <> ".csv
 error = Import[FileNameJoin@{NotebookDirectory[], "error" <> ".csv"},HeaderLines->1];
 
 
-
-
-ListPlot3D[error[1], error[2], error[3]]
-
-
-
-(* ::Code::Initialization::Bold:: *)
-ListPlot3D[{error[All, 1], error[All, 2], error[All, 3]}]
-
-
 ListPlot3D@euclid
 Export[FileNameJoin@{outputDir,"Euklid.pdf"},%];
 ListPlot3D@fastmarching
 Export[FileNameJoin@{outputDir,"FastMarching.pdf"},%];
 ListPlot3D@error
 Export[FileNameJoin@{outputDir,"Error.pdf"},%];
+
+
+
 
 
 
